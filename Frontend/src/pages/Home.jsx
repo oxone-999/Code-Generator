5f1e23b88eba3f97801fcc53f646774e5cf30d69
oxone-999 <anujverma11062002@gmail.com>
2023-06-14 13:14:13 +0530

    minor changes

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { json } from "../inputJson";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 import Button from "@mui/material/Button";
+import "../styles/Home.css";
 
 export const Home = () => {
   console.log(json, json.Entities);
   const navigate = useNavigate();
   return (
     <>
-      <h1>List of Entitities</h1>
+      <div className="home">
+        <h1>List of Entitities</h1>
 
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        {json.Entities.map((entity) => (
-          <>
-            <ListItem>
-            <ListItemText primary={`${entity.name}`}/>
-            <Button variant="contained" color="primary" onClick={() => navigate(`/view/${entity.name}`)}>View {entity.name}</Button>
-            </ListItem>
-          </>
-        ))}
-       </List>
+        <List
+          sx={{ width: "100%", maxWidth: 360 }}
+        >
+          {json.Entities.map((entity) => (
+            <>
+              <ListItem>
+                <ListItemText primary={`${entity.name}`} />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => navigate(`/view/${entity.name}`)}
+                >
+                  View {entity.name}
+                </Button>
+              </ListItem>
+            </>
+          ))}
+        </List>
+      </div>
     </>
   );
 };
